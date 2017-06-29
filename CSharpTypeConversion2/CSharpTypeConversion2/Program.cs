a4c149562b5223eba2f98c04d184021b62374ad6
ChallengerCY <790597004@qq.com>
2017-06-29 18:29:08 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ CSharpTypeConversion2/CSharpTypeConversion2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTypeConversion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C#中值类型和引用类型的转换
+            //值类型转换为引用类型
+            int i = 5;
+            string itostring = i.ToString();
+            Console.WriteLine(itostring);
+            //引用类型转换为值类型
+            //如果输入的字符串不是数字，则会报错
+            int StoI = Convert.ToInt32("100");
+            int StoI2 = Int32.Parse("101");
+            //如果不想处理异常，可以使用tryParse去尝试转换,如果无法转换，会个整形赋值0
+            int Stol3;
+            bool succeed = Int32.TryParse("10k", out Stol3);
+            Console.WriteLine(Stol3);
+            Console.ReadLine();
+        }
+    }
+}
